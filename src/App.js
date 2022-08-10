e2368f602c12a6d0f94748a6ba36adb74b91d328
DarshanBorse <borsedarshan77@gmail.com>
2022-08-10 18:18:35 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -21,58 +21,38 @@ const App = () => {
 
   const checkIsWinner = () => {
     //  checking  winner of the game
-    if (
-      itemArray[0] === itemArray[1] &&
-      itemArray[0] === itemArray[2] &&
-      itemArray[0] !== "empty"
-    ) {
+    if (itemArray[0] === itemArray[1] && itemArray[0] === itemArray[2] && itemArray[0] !== "empty") {
       setWinMessage(`${itemArray[0]} won`);
-    } else if (
-      itemArray[3] !== "empty" &&
-      itemArray[3] === itemArray[4] &&
-      itemArray[4] === itemArray[5]
-    ) {
+    } else if (itemArray[3] !== "empty" && itemArray[3] === itemArray[4] && itemArray[4] === itemArray[5]) {
       setWinMessage(`${itemArray[3]} won`);
-    } else if (
-      itemArray[6] !== "empty" &&
-      itemArray[6] === itemArray[7] &&
-      itemArray[7] === itemArray[8]
-    ) {
+    } else if (itemArray[6] !== "empty" && itemArray[6] === itemArray[7] && itemArray[7] === itemArray[8]) {
       setWinMessage(`${itemArray[6]} won`);
-    } else if (
-      itemArray[0] !== "empty" &&
-      itemArray[0] === itemArray[3] &&
-      itemArray[3] === itemArray[6]
-    ) {
+    } else if (itemArray[0] !== "empty" && itemArray[0] === itemArray[3] && itemArray[3] === itemArray[6]) {
       setWinMessage(`${itemArray[0]} won`);
-    } else if (
-      itemArray[1] !== "empty" &&
-      itemArray[1] === itemArray[4] &&
-      itemArray[4] === itemArray[7]
-    ) {
+    } else if (itemArray[1] !== "empty" && itemArray[1] === itemArray[4] && itemArray[4] === itemArray[7]) {
       setWinMessage(`${itemArray[1]} won`);
-    } else if (
-      itemArray[2] !== "empty" &&
-      itemArray[2] === itemArray[5] &&
-      itemArray[5] === itemArray[8]
-    ) {
+    } else if (itemArray[2] !== "empty" && itemArray[2] === itemArray[5] && itemArray[5] === itemArray[8]) {
+      setWinMessage(`${itemArray[2]} won`);
+    } else if (itemArray[0] !== "empty" && itemArray[0] === itemArray[4] && itemArray[4] === itemArray[8]) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (itemArray[2] !== "empty" && itemArray[2] === itemArray[4] && itemArray[4] === itemArray[6]) {
       setWinMessage(`${itemArray[2]} won`);
     } else if (
       itemArray[0] !== "empty" &&
-      itemArray[0] === itemArray[4] &&
-      itemArray[4] === itemArray[8]
-    ) {
-      setWinMessage(`${itemArray[0]} won`);
-    } else if (
+      itemArray[1] !== "empty" &&
       itemArray[2] !== "empty" &&
-      itemArray[2] === itemArray[4] &&
-      itemArray[4] === itemArray[6]
+      itemArray[3] !== "empty" &&
+      itemArray[4] !== "empty" &&
+      itemArray[5] !== "empty" &&
+      itemArray[6] !== "empty" &&
+      itemArray[7] !== "empty" &&
+      itemArray[8] !== "empty"
     ) {
-      setWinMessage(`${itemArray[2]} won`);
+      setWinMessage(`Try Again`);
     }
   };
 
-  const changeItem = itemNumber => {
+  const changeItem = (itemNumber) => {
     if (winMessage) {
       return toast(winMessage, { type: "success" });
     }
@@ -94,17 +74,13 @@ const App = () => {
         <Col md={6} className="offset-md-3">
           {winMessage ? (
             <div className="mb-2 mt-2">
-              <h1 className="text-success text-uppercase text-center">
-                {winMessage}
-              </h1>
+              <h1 className="text-success text-uppercase text-center">{winMessage}</h1>
               <Button color="success" block onClick={reloadGame}>
                 Reload the game
               </Button>
             </div>
           ) : (
-            <h1 className="text-center text-warning">
-              {isCross ? "Cross" : "Circle"} turns
-            </h1>
+            <h1 className="text-center text-warning">{isCross ? "Cross" : "Circle"} turns</h1>
           )}
           <div className="grid">
             {itemArray.map((item, index) => (
